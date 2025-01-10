4085e573cbe268e5a1fddaf7079f5dc7e33fe8d7
hauntxdd <zusiaoncrack@interia.pl>
2025-01-10 08:05:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
 // main.js
-(async function () {
-  try {
-    const menuModule = await import('https://cdn.jsdelivr.net/gh/hauntxdd/test@main/utils/menu.js?v=' + Date.now());
-    const featuresModule = await import('https://cdn.jsdelivr.net/gh/hauntxdd/test@main/features/bypasschatfilters.js?v=' + Date.now());
+(async function() {
+    const menuModule = await import('https://cdn.jsdelivr.net/gh/hauntxdd/msp2@main/utils/menu.js?v=' + Date.now());
+    const featuresModule = await import('https://cdn.jsdelivr.net/gh/hauntxdd/msp2@main/features/bypasschatfilters.js?v=' + Date.now());
 
-    const createMenu = menuModule.createMenu;
-    const modifyWebSocket = featuresModule.modifyWebSocket;
+    // Tworzenie menu
+    menuModule.createMenu();
 
-    let bypassEnabled = false;
-
-    // create functions
-    const menu = createMenu();
-    const bypassCheckbox = document.getElementById('msp2CheckboxBypass');
-
-    bypassCheckbox.addEventListener('change', () => {
-      bypassEnabled = bypassCheckbox.checked;
-      console.log(`[MSP2] Bypass Unicode ${bypassEnabled ? 'włączony' : 'wyłączony'}`);
-      modifyWebSocket(bypassEnabled);
-    });
-  } catch (error) {
-    console.error('[MSP2] Błąd podczas importu modułów:', error);
-  }
+    // Inicjalizacja funkcji z modułu bypasschatfilters.js
+    if (featuresModule.initBypassChatFilter) {
+        featuresModule.initBypassChatFilter();
+    }
 })();
